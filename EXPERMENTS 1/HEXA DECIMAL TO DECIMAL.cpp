2b9b95587359d442d3c968ec48f4c95b41637792
saithareesh <saithareeshs2161.sse@saveetha.com>
2024-06-27 09:00:20 +0530

    Add files via upload

---
+++ EXPERMENTS 1/HEXA DECIMAL TO DECIMAL.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main ()
+{
+    int n;
+    printf("Enter the Octal decimal number");
+    scanf("%x", & n);
+    printf("The decimal values in :% d",n);
+    return 0;
+    }
